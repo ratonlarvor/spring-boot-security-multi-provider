c99a1f6b61a976e9228ff3d368eee4c1a923b413
ratonlarvor <larvor.yannick@gmail.com>
2019-03-31 14:50:50 +0200

    step(004): Add database provider and add it to security configuration

--- src/main/java/io/opph/example/Application.java
+++ src/main/java/io/opph/example/Application.java
@@ -1,13 +1,30 @@
 package io.opph.example;
 
+import io.opph.example.db.User;
+import io.opph.example.db.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
+    @Component
+    @AllArgsConstructor
+    public class UserLoader implements ApplicationRunner {
+        private PasswordEncoder passwordEncoder;
+        private final UserRepository userRepository;
+
+        public void run(ApplicationArguments args) {
+            userRepository.save(User.builder().login("ykl").password(passwordEncoder.encode("ykl")).withAuthority("ROLE_USER").build());
+        }
+    }
 }
--- src/main/java/io/opph/example/SecurityConfig.java
+++ src/main/java/io/opph/example/SecurityConfig.java
@@ -1,20 +1,30 @@
 package io.opph.example;
 
+import io.opph.example.security.MyUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final MyUserDetailsService myUserDetailsService;
 
     @Override
     @SuppressWarnings("deprecation")
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(databaseAuthenticationProvider());
+
         auth.inMemoryAuthentication()
-                .withUser(User.withDefaultPasswordEncoder().username("user").password("user").roles("USER").build())
-                .withUser(User.withDefaultPasswordEncoder().username("admin").password("admin").roles("USER", "ADMIN").build());
+                .withUser(User.withUsername("user").password(passwordEncoder().encode("user")).roles("USER").build())
+                .withUser(User.withUsername("admin").password(passwordEncoder().encode("admin")).roles("USER", "ADMIN").build());
     }
 
     @Override
@@ -23,5 +33,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and().formLogin()
                 .and().httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider databaseAuthenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(myUserDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
